fd8721fc770f5bd49ad44fd8bda88243af3263f6
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-02-20 20:39:54 +0500

    Learnd how to use pyttsx3 module for text to speach

---
+++ voice.py
@@ -0,0 +1,5 @@
+# In order to run this program you have to install pyttsx3 module
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("There are a lot of languages but Python is the most fun language to learn.")
+engine.runAndWait()
