a07166a36e6cd0c893ba59da157db8476c15b8ed
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-02-20 20:57:21 +0500

    Learned about data types

---
+++ chepter_2/datatypes.py
@@ -0,0 +1,11 @@
+# There are 5 types of data types in python that are following:
+# 1- Integer
+firstNumber= 12
+# 2- Float
+secondNumber= 12.5
+# 3- String
+fullName="Subhan Ali"
+# 4- Boolean
+isSleeping = False
+# 5- None
+demoes=None
