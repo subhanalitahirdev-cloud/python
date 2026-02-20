8b7701964ca1bf2215c6ccde9378671b83eb86f0
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-02-20 20:22:53 +0500

    Used os module

---
+++ os.py
@@ -0,0 +1,4 @@
+import os
+# In this program I used the os module for getting the current directory and list of all of the files that are in that directory
+print(os.getcwd())
+print(os.listdir())
---
+++ practice.py
@@ -0,0 +1,1 @@
+print("Twinkle Twinkle little Start\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky.");
