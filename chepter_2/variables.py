379ff004607ccfd2d42ca6ae08496240f925bc49
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-02-20 20:49:57 +0500

    Learned about variables

---
+++ chepter_2/variables.py
@@ -0,0 +1,6 @@
+# Variables are the containers that are used to store values.
+firstName = "Subhan"
+secondName = "Ali"
+job = "Web Developer"
+age = 20
+print("My name is ",firstName," ",secondName,"I am a ",job," My age is ", age)
