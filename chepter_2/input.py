e29ebfb5e4ecb9c3d5b9309b5089740d167b025d
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-02-21 10:26:07 +0500

    learned about input function

---
+++ chepter_2/input.py
@@ -0,0 +1,3 @@
+a=input("Please enter the first number")
+b=input("Please enter the second number")
+print("The first number that the user entered is",a,"and the second number that the user entered is",b,"and the sum of both of these numbers is",int(a)+int(b))
